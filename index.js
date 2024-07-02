809c846f7bb9cd7ed399036c70821b214f6a36a9
asif-sidd9768 <70014894+asif-sidd9768@users.noreply.github.com>
2024-07-02 17:24:37 +0530

    fix: 'add' function supports different delimeters

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const add = (numbers) => {
-    if(numbers === ""){
+const add = (input) => {
+    if(input === ""){
         return 0
     }
-    return numbers.split(/,|\n/).reduce((acc, curr) => acc+Number(curr), 0)
+    const regex = /^\/\/(.*?)\n/;
+    const match = input.match(regex);
+
+    let reg
+    if(match){
+        input = input.split("\n")[1]
+        reg = new RegExp(`,|\\n|${match[1]}`)
+    }else {
+        reg = new RegExp(`,|\\n`)
+    }
+
+    return input.split(reg).reduce((acc, curr) => acc+Number(curr), 0)
 }
+
+add("//;\n1;2")
 
 module.exports = add
