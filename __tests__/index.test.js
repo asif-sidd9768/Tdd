25b337470f4047ae508ac1e0f4a658cd7fc37af1
asif-sidd9768 <70014894+asif-sidd9768@users.noreply.github.com>
2024-07-02 16:16:22 +0530

    test: added test to for any amount of numbers in input

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,4 +4,8 @@ describe('Add calculator', () => {
     test('should return 0 for empty string', () => {
         expect(add("")).toBe(0)
     });
+
+    test('should handle any amount of numbers', () => {
+        expect(add("1,2,3,4,5")).toBe(15)
+    });
 });
