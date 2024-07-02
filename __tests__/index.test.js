d5e48377c7ac4d3334f0818fdc3c5091038d41ea
asif-sidd9768 <70014894+asif-sidd9768@users.noreply.github.com>
2024-07-02 16:22:15 +0530

    test: added to handle new lines between numbers

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,4 +8,8 @@ describe('Add calculator', () => {
     test('should handle any amount of numbers', () => {
         expect(add("1,2,3,4,5")).toBe(15)
     });
+
+    test('should handle new lines between numbers', () => {
+        expect(add("1\n2,3")).toBe(6)
+    });
 });
