9d28f73d3b55e435e6774e8bad383eeab33c1ddb
asif-sidd9768 <70014894+asif-sidd9768@users.noreply.github.com>
2024-07-02 18:55:39 +0530

    fix: 'add' function to allow multiple delimeters

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const add = (input) => {
-    if(input === ""){
-        return 0
+    if (input === "") {
+        return 0;
     }
-    const regex = /^\/\/(.*?)\n/;
+
+    const regex = /^\/\/(\[.*?\]|.)\n/;
     const match = input.match(regex);
 
-    let reg
-    if(match){
-        input = input.split("\n")[1]
-        reg = new RegExp(`,|\\n|${match[1]}`)
-    }else {
-        reg = new RegExp(`,|\\n`)
-    }
-
-    if(input.split(reg).some(n => n < 0)){
-        const negativeNumbers = input.split(reg).filter(n => n < 0).join(", ")
-        console.log(negativeNumbers)
-        throw new Error(`negative numbers not allowed ${negativeNumbers}`)
-    }
-
-    return input.split(reg).reduce((acc, curr) => {
-        const currNum = Number(curr)
-        if(currNum >= 1000){
-            return acc
+    let reg;
+    if (match) {
+        let delimiters;
+        if (match[1].startsWith('[')) {
+            // Multiple delimiters
+            delimiters = match[1]
+                .slice(1, -1)
+                .split('][')
+                .map(d => d.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
+        } else {
+            // Single character delimiter
+            delimiters = [match[1].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')];
         }
-        return acc + currNum
-    }, 0)
-}
+        input = input.split("\n")[1];
+        reg = new RegExp(delimiters.join('|') + '|,|\\n');
+    } else {
+        reg = new RegExp(`,|\\n`);
+    }
 
-module.exports = add
+    const numbers = input.split(reg).map(Number);
+
+    const negativeNumbers = numbers.filter(n => n < 0);
+    if (negativeNumbers.length > 0) {
+        throw new Error(`negative numbers not allowed ${negativeNumbers.join(", ")}`);
+    }
+
+    return numbers.reduce((acc, curr) => {
+        if (curr >= 1000) {
+            return acc;
+        }
+        return acc + curr;
+    }, 0);
+};
+
+module.exports = add;
