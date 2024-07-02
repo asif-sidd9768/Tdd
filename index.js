41dc107658699469922b2bb1822cae92862b930f
asif-sidd9768 <70014894+asif-sidd9768@users.noreply.github.com>
2024-07-02 16:23:18 +0530

    fix: 'add' function to handle new lines between numbers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const add = (numbers) => {
     if(numbers === ""){
         return 0
     }
-    return numbers.split(",").reduce((acc, curr) => acc+Number(curr), 0)
+    return numbers.split(/,|\n/).reduce((acc, curr) => acc+Number(curr), 0)
 }
 
 module.exports = add
